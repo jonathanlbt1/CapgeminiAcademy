1766403347925cc2583870ae0711ff589d786834
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2022-02-18 18:43:13 -0300

    Aleracoes no exercicio 3

--- src/Question_3/Question_3.java
+++ src/Question_3/Question_3.java
@@ -7,29 +7,31 @@ import java.util.Scanner;
 public class Question_3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("*****Seja muito bem vindo ao contador de anagramas!*****\n");
+        System.out.print("Para iniciarmos, digite por favor uma palavra ou string: ");
         String anagrama = scan.nextLine();
 
-        subString(anagrama);
+        funcaoContaAnagramas(anagrama);
         scan.nextInt();
     }
 
-    static void subString(String s){
-        HashMap<String, Integer> map= new HashMap<>();
+    static void funcaoContaAnagramas(String s){
+        HashMap<String, Integer> mapeandoAnagramas = new HashMap<>();
 
         for(int i = 0; i < s.length(); i++){
             for(int j = i; j < s.length(); j++){
                 char[] caractere = s.substring(i, j+1).toCharArray();
                 Arrays.sort(caractere);
                 String valor = new String(caractere);
-                if (map.containsKey(valor))
-                    map.put(valor, map.get(valor)+1);
+                if (mapeandoAnagramas.containsKey(valor))
+                    mapeandoAnagramas.put(valor, mapeandoAnagramas.get(valor)+1);
                 else
-                    map.put(valor, 1);
+                    mapeandoAnagramas.put(valor, 1);
             }
         }
         int contagemDeAnagramas = 0;
-        for(String key: map.keySet()){
-            int n = map.get(key);
+        for(String key: mapeandoAnagramas.keySet()){
+            int n = mapeandoAnagramas.get(key);
             contagemDeAnagramas += (n * (n-1))/2;
         }
         System.out.println(contagemDeAnagramas);
